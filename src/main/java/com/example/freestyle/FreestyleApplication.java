22aa0fd3c2ccfd1cb9f5cbb5e518bdd00039ec6d
Henry Akintoye <talk2henrie@gmail.com>
2022-06-04 22:33:02 +0100

    initial commit

---
+++ src/main/java/com/example/freestyle/FreestyleApplication.java
@@ -0,0 +1,13 @@
+package com.example.freestyle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreestyleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreestyleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/freestyle/FreestyleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freestyle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreestyleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
